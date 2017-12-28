856e3ab40972ca6c96aa2018238c7f1b0f7d844f
Isnu Awalin <nueyassa@gmail.com>
2017-12-28 21:47:55 +0700

    nue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   L A R A V E L
+                   Welcome To The Laravel
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
